f29f87c41d08fd783d889f17d5366b1d30cb3a44
casshernsinz <66576864+casshernsinz@users.noreply.github.com>
2020-07-20 15:18:26 -0500

    Naming Scheme Changes
    Added "s" to AdminViewModels
    
    Created ContactViewModels
    
    Rerouted img(s) to ~/Content/
    
    Used @Url.Action for links to Homepage
    
    Created @Url.Action for future links Books, Genres, Authors, Publishers
    
    Renamed DropDown One to Admin Functions & replaced links with @Url.Actions for Users Admin and Roles Admin
    
    Commented out DropDown Two
    
    Added Copyright David Hall and DateTime.Now.Year to automatically update the year
    
    Added @RenderSection("scripts", false) at the bottom of the <body></body>

---
+++ MVC2EFSecured.UI.MVC/Models/ContactViewModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;//Added for validation&formatting
+
+namespace MVC2EFSecured.UI.MVC.Models
+{
+    public class ContactViewModels
+    {
+        [Required(ErrorMessage = "* Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Email is Required")]
+        //If you need to use a regular expression to match a pattern, you can use the syntax below
+        //[RegularExpression("ExpressionPatternHere",ErrorMessage = "Message Here")]
+        [DataType(DataType.EmailAddress)]//uses a built-in regex pattern to validate user input
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Subject is Required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Message is Required")]
+        [UIHint("MultilineText")]//Provides a text area (multiline text box) for this property
+        public string Message { get; set; }
+    }
+}
